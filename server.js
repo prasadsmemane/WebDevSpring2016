7e03ce55a9c624f7858911e065db3ba3e6f96267
prasadsmemane <prasadsmemane@gmail.com>
2016-02-02 02:22:44 -0500

    Moved index.html to public dir

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('/hello', function(req, res){
+    res.send('Hello World');
+});
+
+
+//Connection configuration for local and openshift
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
 app.listen(port, ipaddress);
