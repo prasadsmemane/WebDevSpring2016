3d513234d23153232ed393a0788b0c6c3cd911bb
prasadsmemane <prasadsmemane@gmail.com>
2016-03-11 05:15:38 -0500

    Flipped the order of app use and app options for cors

--- server.js
+++ server.js
@@ -2,31 +2,13 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
-app.options('*', cors());
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
+app.options('*', cors());
+
 app.get('/hello', function(req, res){
     res.send('Hello World');
-});
-
-app.use(function(req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://webdev2016-memaneprasad.rhcloud.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
 });
 
 //Connection configuration for local and openshift
