2c1302d8884faf61279417d3bb9d1dfa3402ab7a
prasadsmemane <prasadsmemane@gmail.com>
2016-03-11 04:38:58 -0500

    Removed cors dependency

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
-var cors = require('cors')
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.use(cors());
 
-app.options('*', cors());
 app.get('/hello', function(req, res){
     res.send('Hello World');
 });
@@ -15,14 +12,14 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://webdev2016-memaneprasad.rhcloud.com');
 
     // Request methods you wish to allow
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    //res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
